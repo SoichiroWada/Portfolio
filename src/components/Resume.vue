9f78881d3dec7262294f14bff9955197e4e1ea3d
SoichiroWada <soichirowada@ipmulticast.net>
2022-06-10 02:42:07 +0900

    updated resume EN

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -11,38 +11,40 @@
       </ul>
       <h2 class="reh2">SKILLS</h2><hr>
       <ul class="retext">
-        <li>Frontend: JavaScript (React/Redux, Vue.js, jQuery, ES6), CSS(Flexbox, Grid, SASS), HTML</li>
-        <li>Backend: Node.js, Express, MongoDB, Mongoose, Firebase</li>
-        <li>Linux (or BSD): Node.js, Express and MongoDB are running on Ubuntu 18.04 LTS server at home. 
-          Test programming on Node.js and basic operations are made everyday on this server. (May 2017 - )</li>
-        <li>Internet (IP network): have basic knowledge owing to the working experience in Verizon Enterprise for 8.5 years</li>
-        <li>Others: Capable of consulting security products, Google G Suite with private domain and email.</li>
+        <li>Frontend: JavaScript/TypeScript, React, Vue.js, HTML, CSS</li>
+        <li>Backend: Node.js, Express, MongoDB, Mongoose, Firebase, AWS</li>
+        <li>Linux: basic skills for operation on Ubuntu 18.04 LTS Server (running at home 2017 - )</li>
+        <li>Internet (IP network): TCP/IP, CIDR, DNS, etc, have basic skills as Software Engineer</li>
+        <li>AWS: API Gateway, Lambda, DynamoDB, S3, Cognito, IoT Core, EC2 enables quick launch mini system</li>
+        <li>GCP: Firebase(Hosting, Database, Functions, Authentication), Google Maps</li>
       </ul>
       <h2 class="reh2">PERSONAL PROJECTS</h2><hr>
       <ul class="retext">
-        <h3 class="reh3">freeCodeCamp’s Front End Projects (March - Nov 2017)</h3>
+        <h3 class="reh3">freeCodeCamp’s Front End Projects (Mar2017 - Nov 2017)</h3>
           <li>Completed Free Code Camp’s Front End Development Program at Nov 2017</li>
-          <li>Created Game, Chat, Pomodoro Clock and Calculator app by JavaScript.</li>
         <h3 class="reh3">Node School (2018)</h3>
           <li>Learnyounode, How to npm.</li>
         <h3 class="reh3">Node.js Design Patterns second edition (2018)</h3>
           <li>Understanding of technical elements, Non-blocking IO, Asynchronous control flow, Streams, and others</li>
-        <h3 class="reh3">The Net Ninja’s coding tutorials (2017 - 2019)</h3>
-          <li>Total 17 courses completed --- React & Redux, React Redux & Firebase App, Node.js, MongoDB, REST API, React, Webpack, WebSockets, OAuth, Vue.js</li>
-          <li>Created Todo List, Ninja Recipe, Chat, Ninja Map and other apps by React/Redux or VUE CLI and Firebase.</li>
+        <h3 class="reh3">The Net Ninja’s coding tutorials (2016 - 2022)</h3>
+          <li>	Completed 25 courses of Node.js, TypeScript, React, Next, Flutter, Git/GitHub, MongoDB, REST API, Graph QL, Webpack, WebSockets, OAuth, Vue.js, and Firebase/Firestore and others.</li>
         <h3 class="reh3">HackerRank (Feb 2019 - )</h3>
-          <li>Started and continue trying programming challenge by Node.js (JavaScript)</li>
+          <li>Continue trying programming challenge by Node.js (JavaScript)</li>
       </ul>
       <h2 class="reh2">WORK EXPERIENCE</h2><hr>
       <ul class="retext">
-        <h3 class="reh3">Freelance Software Engineer (Sep 2016 - present, 2.5 years)</h3>
-          <li>Provided ICT solutions to small companies such as GSuite configuration, WordPress editing, updating / modifying Website</li>
-        <h3 class="reh3">Network Value Components Ltd (March 2016 – August 2016, 6 months)</h3>
+        <h3 class="reh3">KDDI Corporation (May 2019 - May 2022, 3 years)</h3>
+          <li>Provided technical proposal to KDDI enterprise customers for accelerating customer’s DX activities</li>
+          <li>Developed small applications to clarify new business idea</li>
+        <h3 class="reh3">Freelance Software Engineer (Sep 2016 - Apr 2019, 2.5 years)</h3>
+          <li>Developed web system for employment agency, EC site of livingware manufacturer</li>
+          <li>Provided ICT solutions to small companies such as GSuite configuration, WordPress editing, updating Website</li>
+        <h3 class="reh3">Network Value Components Ltd (Mar 2016 – Aug 2016, 6 months)</h3>
           <li>Provided sales and marketing activities for security products such as FortiGate, vARMOUR and ThreatSTOP</li>
-        <h3 class="reh3">Tamago-DB Corp (April 2015 – Oct 2015, 6 months)</h3>
-          <li>Provided building marketing Website for selling cloud-based software service to recruiting agencies</li>
-        <h3 class="reh3">Mobilitysupport LLC (June 2011 – March 2015, 3.7 years, owned by myself)</h3>
-          <li>Provided business proposal and built Website for selling care equipments to care service providers (2011 ~ 2014).</li>
+        <h3 class="reh3">Tamago-DB Corp (Apr 2015 – Oct 2015, 6 months)</h3>
+          <li>Developed marketing Website having linkage with MailChimp for selling cloud-based software service</li>
+        <h3 class="reh3">Mobilitysupport LLC (Jun 2011 – Mar 2015, 3.7 years)</h3>
+          <li>Developed EC site (front end) for selling care equipment to care service providers (2011 ~ 2014)</li>
           <li>Provided IT consulting activities for small companies using cloud services including Google Apps, AWS.</li>
         <h3 class="reh3">AIU insurance company (Aug 2009 - April 2011, 1.8 years)</h3>
           <li>Provided consulting activities for liability insurance, accident insurance to small - medium sized companies</li>
@@ -51,8 +53,8 @@
         <h3 class="reh3">ImageONE Co., Ltd (Oct 1998 - Feb 2000, 1.5 years, Trading company for satellite data receiving system)</h3>
           <li>Provided sales and marketing activities for Data Receiving System of NOAA Satellite, ASD Spectroradiometers.</li>
         <h3 class="reh3">Fuji Xerox Co., Ltd (Apr 1988 - Sep 1998, total 10.5 years)</h3>
-          <li>Provided marketing activities for Multifunction Laser Printer (4.5 years)</li>
-          <li>Provided business planning activities for corporate 5 year-mid-term business plan (3 years)</li> 
+          <li>Provided marketing activities for Multifunction Laser Printers (4.5 years)</li>
+          <li>Provided business planning activities in HQ(3 years)</li> 
           <li>Provided marketing activities for AI Software (Smalltalk-80 and Interlisp-D) and UNIX Workstations (3 years)</li> 
       </ul>
       </div>
